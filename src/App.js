8f32f078b3e18097128c0def04fc133e8e7a406b
svonk <example@mail.ru>
2021-08-12 12:18:13 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './styles/App.css'
+import PostsList from "./Components/PostsList";
+import PostForm from "./Components/PostForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    let [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'}
+    ])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(el => el.id !== post.id))
+    }
+
+
+    return (
+        <div className='App'>
+            <PostForm create={createPost}/>
+            {posts.length !== 0
+                ?
+                <PostsList remove={removePost} posts={posts} title='Posts list 1'/>
+                :
+                <h1 style={{textAlign: 'center'}}>Post's List is empty</h1>
+            }
+
+        </div>
+    );
 }
+
 
 export default App;
